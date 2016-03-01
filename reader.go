77724fd6d57363882848a4415151ff1df38f7c0d
Dan Willoughby <dan@spacemonkey.com>
2016-03-01 10:04:42 -0700

    Add Reader interface

--- reader.go
+++ reader.go
@@ -2,19 +2,26 @@ package stream
 
 import "io"
 
-// Reader is a concurrent-safe Stream Reader.
-type Reader struct {
+type Reader interface {
+	Name() string
+	ReadAt(p []byte, off int64) (n int, err error)
+	Read(p []byte) (n int, err error)
+	Close() error
+}
+
+// StreamReader is a concurrent-safe Stream Reader.
+type StreamReader struct {
 	s    *Stream
 	file File
 }
 
 // Name returns the name of the underlying File in the FileSystem.
-func (r *Reader) Name() string { return r.file.Name() }
+func (r *StreamReader) Name() string { return r.file.Name() }
 
 // ReadAt lets you Read from specific offsets in the Stream.
 // ReadAt blocks while waiting for the requested section of the Stream to be written,
 // unless the Stream is closed in which case it will always return immediately.
-func (r *Reader) ReadAt(p []byte, off int64) (n int, err error) {
+func (r *StreamReader) ReadAt(p []byte, off int64) (n int, err error) {
 	r.s.b.RLock()
 	defer r.s.b.RUnlock()
 
@@ -45,7 +52,7 @@ func (r *Reader) ReadAt(p []byte, off int64) (n int, err error) {
 
 // Read reads from the Stream. If the end of an open Stream is reached, Read
 // blocks until more data is written or the Stream is Closed.
-func (r *Reader) Read(p []byte) (n int, err error) {
+func (r *StreamReader) Read(p []byte) (n int, err error) {
 	r.s.b.RLock()
 	defer r.s.b.RUnlock()
 
@@ -76,7 +83,7 @@ func (r *Reader) Read(p []byte) (n int, err error) {
 
 // Close closes this Reader on the Stream. This must be called when done with the
 // Reader or else the Stream cannot be Removed.
-func (r *Reader) Close() error {
+func (r *StreamReader) Close() error {
 	defer r.s.dec()
 	return r.file.Close()
 }
